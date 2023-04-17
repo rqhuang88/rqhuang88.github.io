aa20a72ac9c4c56c55c057a99141a29351833b71
TBSI2f <127710727+TBSI2f@users.noreply.github.com>
2023-04-17 15:22:54 +0800

    Add files via upload

---
+++ js/tabbar.js
@@ -0,0 +1,13 @@
+function changeCategory(evt, Name) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(Name).style.display = "block";
+    evt.currentTarget.className += " active";
+}
